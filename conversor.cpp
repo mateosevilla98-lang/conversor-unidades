5b36b585d35de2d49d91eecc5dc8b3fb2b92e17b
Kevin Joel Sinchiguano <21joel21sinchiguano@gmail.com>
2026-08-09 00:38:10 -0500

    Integra conversiones de peso en el menu principal

--- conversor.cpp
+++ conversor.cpp
@@ -16,6 +16,33 @@ void gramosALibras() {
 
 int main() {
     int opcion;
+
+    cout << "--- Conversor de Unidades ---" << endl;
+    cout << "1. Metros a Kilometros" << endl;
+    cout << "2. Metros a Millas" << endl;
+    cout << "3. Gramos a Kilogramos" << endl;
+    cout << "4. Gramos a Libras" << endl;
+    cout << "5. Salir" << endl;
+    cout << "Seleccione una opcion: ";
+    cin >> opcion;
+
+    if (opcion == 1) {
+        // metrosAKilometros();
+    } else if (opcion == 2) {
+        // metrosAMillas();
+    } else if (opcion == 3) {
+        gramosAKilogramos();
+    } else if (opcion == 4) {
+        gramosALibras();
+    } else if (opcion == 5) {
+        cout << "Saliendo del programa..." << endl;
+    } else {
+        cout << "Opcion no valida." << endl;
+    }
+
+    return 0;
+}
+    int opcion;
     cout << "--- Conversor de Unidades ---" << endl;
     cout << "1. Salir" << endl;
     cout << "Seleccione una opcion: ";
